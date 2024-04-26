47053246bff4f258fe7c8aad0901f85f628f517d
Santhosh <imsanthosh03@gmail.com>
2024-04-26 17:20:50 +0530

    Method Added in Global Exception

--- src/main/java/com/jparelation/exception/ExceptionHandler.java
+++ src/main/java/com/jparelation/exception/ExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ExceptionHandler {
 	
+	public void getMessage() {
+		System.out.println("Hello");
+	}
 	
 
 }
